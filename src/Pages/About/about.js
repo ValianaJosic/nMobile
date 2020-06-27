d44d6686dd59a6bb51f1785bb2450a6216603275
Valiana <valiana.josic@gmail.com>
2020-06-27 16:15:11 -0400

    about page content CSS

--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -6,7 +6,7 @@ import Banner from '../../Assets/Images/banner1.jpg'
 const About = () => {
   return (
     <div className="about">
-        <img src={Banner} alt='banner'className='about-img'></img>
+        <div className='about-img-div'><img src={Banner} alt='banner'className='about-img'></img></div>
       <p className="about-intro">How will your small business succeed in this changing world?</p>
        <div className="intro-div"><p className="about-info">During these difficult times small businesses are the ones affected most. As we shift more towards a digital world , the only way for your business to thirve is to adapt to the changes. nMobile is an ERP Software that helps manage your day-to-day processes virtually. Extend your business beyond your office walls with a personalized App tailored to your industry. Whether it’s retail, restaurant, warehouse, medical and more, we can help you make the transition and launch your small enterprise into the new tech world.</p>
        </div> 
